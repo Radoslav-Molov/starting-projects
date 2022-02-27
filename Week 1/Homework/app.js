b9c6be9647a99fcdfef989963740e9e24da6e356
MadeInSmolyan <79079476+MadeInSmolyan@users.noreply.github.com>
2022-02-27 19:03:21 +0200

    made the fetch reusable

--- Week 1/Homework/app.js
+++ Week 1/Homework/app.js
@@ -5,49 +5,95 @@ const searchInput = document.getElementById("search-input");
 const sectionEl = document.getElementById("main");
 const asideBtns = document.querySelectorAll(".aside-btn");
 const asideSectionEl = document.getElementById("aside-content");
+
 searchInput.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     getBooks();
   }
 });
+
 asideBtns.forEach((button) => {
   button.addEventListener("click", exampleBooks);
 });
+exampleIsUsed = false;
 
 exampleBooks();
 
-function getBooks(e) {
+function exampleBooks(e) {
+  asideSectionEl.innerHTML = "";
+
+  let url = "";
+
+  if (e === undefined) {
+    url = `https://www.googleapis.com/books/v1/volumes?q=google`;
+  } else {
+    let query = e.target.value;
+    url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+  }
+
+  exampleIsUsed = true;
+
+  getData(url);
+}
+
+function getBooks() {
   sectionEl.innerHTML = "";
 
   let inputValue = searchInput.value;
-  fetch(`https://www.googleapis.com/books/v1/volumes?q=${inputValue}`)
+  let url = `https://www.googleapis.com/books/v1/volumes?q=${inputValue}`;
+
+  exampleIsUsed = false;
+  console.log(exampleIsUsed);
+
+  getData(url);
+}
+
+function getData(url) {
+  fetch(url)
     .then((resp) => resp.json())
     .then((response) => {
       response.items.forEach((book) => {
         let data = book.volumeInfo;
 
         let card = document.createElement("div");
-        let cardImg = document.createElement("img");
-        let cardTitle = document.createElement("h2");
-        let cardAuthor = document.createElement("h3");
-        let cardPublisher = document.createElement("span");
-        let cardYear = document.createElement("span");
-        let cardCategory = document.createElement("span");
-        let cardDescription = document.createElement("span");
-        let moreBtn = document.createElement("button");
 
-        let subDescription = data.description.substring(0, 170);
+        let cardImg = document.createElement("img");
+        card.innerHTML = `<img src=${data.imageLinks.thumbnail}>`;
+
+        let cardTitle = document.createElement("h2");
+        cardTitle.textContent = data.title;
+
+        let cardAuthor = document.createElement("h3");
+        cardAuthor.innerHTML = `<strong>Authors</strong>: ${data.authors}`;
+
+        let cardPublisher = document.createElement("span");
+        cardPublisher.innerHTML = `<strong>Publisher</strong>: ${data.publisher}`;
+
+        let cardYear = document.createElement("span");
+        cardYear.innerHTML = `<strong>Date</strong>: ${data.publishedDate}`;
+
+        let cardCategory = document.createElement("span");
+        cardCategory.innerHTML = `<strong>Category</strong>: ${data.categories}`;
+
+        let cardDescription = document.createElement("span");
+
+        let subDescription = "";
+
+        if (data.description !== undefined) {
+          data.description.substring(0, 140);
+        }
+
+        cardDescription.innerHTML = `<strong>Description</strong>: ${subDescription}...`;
+        cardDescription.className = "description-span";
+
+        let moreBtn = document.createElement("button");
+        let favourites = document.createElement("button");
+
         moreBtn.textContent = "See more";
         moreBtn.addEventListener("click", moreDescription);
 
-        card.innerHTML = `<img src=${data.imageLinks.thumbnail}>`;
-        cardTitle.textContent = data.title;
-        cardAuthor.innerHTML = `<strong>Authors</strong>: ${data.authors}`;
-        cardPublisher.innerHTML = `<strong>Publisher</strong>: ${data.publisher}`;
-        cardYear.innerHTML = `<strong>Date</strong>: ${data.publishedDate}`;
-        cardCategory.innerHTML = `<strong>Category</strong>: ${data.categories}`;
-        cardDescription.innerHTML = `<strong>Description</strong>: ${subDescription}...`;
-        cardDescription.className = "description-span";
+        favourites.textContent = "Add to Favourites";
+        favourites.id = "fav-btn";
 
         function moreDescription(e) {
           cardDescription.innerHTML = `<strong>Description</strong>: ${data.description}...`;
@@ -70,69 +116,23 @@ function getBooks(e) {
         card.appendChild(cardCategory);
         card.appendChild(cardDescription);
         cardDescription.appendChild(moreBtn);
+        card.appendChild(favourites);
 
-        sectionEl.appendChild(card);
+        if (exampleIsUsed === true) {
+          asideSectionEl.appendChild(card);
+          if (asideSectionEl.children.length >= 2) {
+            throw "Stop";
+          }
+        } else {
+          sectionEl.appendChild(card);
+        }
       });
     })
     .catch((err) => {
       console.log(err);
-      const errorMsg = document.createElement("h2");
-      errorMsg.textContent =
-        "There was an error fetching the data, try with another keyword.";
-      sectionEl.appendChild(errorMsg);
-    });
-}
-
-function exampleBooks(e) {
-  asideSectionEl.innerHTML = "";
-
-  let url = "";
-
-  if (e === undefined) {
-    url = `https://www.googleapis.com/books/v1/volumes?q=google`;
-  } else {
-    let query = e.target.value;
-    url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
-  }
-
-  fetch(`${url}`)
-    .then((resp) => resp.json())
-    .then((data) => {
-      let arr = data.items;
-      for (let i = 0; i < 3; i++) {
-        let asideCard = document.createElement("div");
-        let asideCardImg = document.createElement("img");
-        let asideCardTitle = document.createElement("h2");
-        let asideCardAuthor = document.createElement("h3");
-        let asideCardPublisher = document.createElement("span");
-        let asideCardYear = document.createElement("span");
-        let asideCardCategory = document.createElement("span");
-        let asideCardDescription = document.createElement("span");
-
-        asideCard.innerHTML = `<img src=${arr[i].volumeInfo.imageLinks.thumbnail}>`;
-        asideCardTitle.textContent = arr[i].volumeInfo.title;
-        asideCardAuthor.textContent = arr[i].volumeInfo.authors;
-        asideCardPublisher.textContent = arr[i].volumeInfo.publisher;
-        asideCardYear.textContent = arr[i].volumeInfo.publishedDate;
-        asideCardCategory.textContent = arr[i].volumeInfo.categories;
-        asideCardDescription.textContent = arr[i].volumeInfo.description;
-
-        asideCard.appendChild(asideCardImg);
-        asideCard.appendChild(asideCardTitle);
-        asideCard.appendChild(asideCardAuthor);
-        asideCard.appendChild(asideCardPublisher);
-        asideCard.appendChild(asideCardYear);
-        asideCard.appendChild(asideCardCategory);
-        asideCard.appendChild(asideCardDescription);
-
-        asideSectionEl.appendChild(asideCard);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      const errorMsg = document.createElement("h2");
-      errorMsg.textContent =
-        "There was an error fetching the data, try with another keyword.";
-      asideSectionEl.appendChild(errorMsg);
+      // const errorMsg = document.createElement("h2");
+      // errorMsg.textContent =
+      //   "There was an error fetching the data, try with another keyword.";
+      // sectionEl.appendChild(errorMsg);
     });
 }
