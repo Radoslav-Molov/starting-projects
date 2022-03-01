6c649db98f5dc79791ba737af5d6677cb24d11d7
MadeInSmolyan <79079476+MadeInSmolyan@users.noreply.github.com>
2022-03-01 20:50:36 +0200

    made post call

--- Week 1/Homework/app.js
+++ Week 1/Homework/app.js
@@ -1,3 +1,5 @@
+import page from "../node_modules/page/page.mjs";
+
 const searchForm = document
   .getElementById("form-wrapper")
   .addEventListener("submit", getUrl);
@@ -8,7 +10,13 @@ const sectionEl = document.getElementById("main");
 const asideBtns = document.querySelectorAll(".aside-btn");
 const asideSectionEl = document.getElementById("aside-content");
 
-let searchBookResult = [];
+let favouriteBooks = [];
+page(
+  "/",
+  getData.bind(null, "https://www.googleapis.com/books/v1/volumes?q=google")
+);
+// page("/favourite", getFavourites());
+page.start();
 
 searchInput.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
@@ -36,77 +44,7 @@ function getData(url) {
     .then((resp) => resp.json())
     .then((response) => {
       response.items.forEach((book) => {
-        let id = book.id;
-
-        let data = book.volumeInfo;
-
-        let card = document.createElement("div");
-
-        let cardImg = document.createElement("img");
-        card.innerHTML = `<img src=${data.imageLinks.thumbnail}>`;
-
-        let cardTitle = document.createElement("h2");
-        cardTitle.textContent = data.title;
-
-        let cardAuthor = document.createElement("h3");
-        cardAuthor.innerHTML = `<strong>Authors</strong>: ${data.authors}`;
-
-        let cardPublisher = document.createElement("span");
-        cardPublisher.innerHTML = `<strong>Publisher</strong>: ${data.publisher}`;
-
-        let cardYear = document.createElement("span");
-        cardYear.innerHTML = `<strong>Date</strong>: ${data.publishedDate}`;
-
-        let cardCategory = document.createElement("span");
-        cardCategory.innerHTML = `<strong>Category</strong>: ${data.categories}`;
-
-        let cardDescription = document.createElement("span");
-
-        let subDescription;
-
-        if (data.description !== undefined) {
-          subDescription = data.description.substring(0, 140);
-        }
-
-        cardDescription.innerHTML = `<strong>Description</strong>: ${subDescription}...`;
-        cardDescription.className = "description-span";
-
-        let moreBtn = document.createElement("button");
-        let favourites = document.createElement("button");
-
-        moreBtn.textContent = "See more";
-        moreBtn.addEventListener("click", moreDescription);
-
-        favourites.textContent = "Add to Favourites";
-        favourites.id = "fav-btn";
-        favourites.addEventListener("click", addToFavourites);
-
-        function moreDescription(e) {
-          cardDescription.innerHTML = `<strong>Description</strong>: ${data.description}...`;
-          let lessBtn = document.createElement("button");
-          lessBtn.textContent = "Show less";
-          cardDescription.appendChild(lessBtn);
-          lessBtn.addEventListener("click", lessDescription);
-        }
-
-        function lessDescription(e) {
-          cardDescription.innerHTML = `<strong>Description</strong>: ${subDescription}...`;
-          cardDescription.appendChild(moreBtn);
-        }
-
-        card.appendChild(cardImg);
-        card.appendChild(cardTitle);
-        card.appendChild(cardAuthor);
-        card.appendChild(cardPublisher);
-        card.appendChild(cardYear);
-        card.appendChild(cardCategory);
-        card.appendChild(cardDescription);
-        cardDescription.appendChild(moreBtn);
-        card.appendChild(favourites);
-
-        sectionEl.appendChild(card);
-        searchBookResult.push({ id });
-        console.log(searchBookResult);
+        createBook(book);
       });
     })
     .catch((err) => {
@@ -118,6 +56,125 @@ function getData(url) {
     });
 }
 
-function addToFavourites(e) {
-  console.log(e);
+function createBook(book) {
+  let data = "";
+  let imgPath = book;
+  if (book.title === undefined) {
+    data = book.volumeInfo;
+    imgPath = book.volumeInfo.imageLinks;
+  } else {
+    data = book;
+  }
+  let card = document.createElement("div");
+
+  let cardImg = document.createElement("img");
+  card.innerHTML = `<img src=${imgPath.thumbnail}>`;
+
+  let cardTitle = document.createElement("h2");
+  cardTitle.textContent = data.title;
+
+  let cardAuthor = document.createElement("h3");
+  cardAuthor.innerHTML = `<strong>Authors</strong>: ${data.authors}`;
+
+  let cardPublisher = document.createElement("span");
+  cardPublisher.innerHTML = `<strong>Publisher</strong>: ${data.publisher}`;
+
+  let cardYear = document.createElement("span");
+  cardYear.innerHTML = `<strong>Date</strong>: ${data.publishedDate}`;
+
+  let cardCategory = document.createElement("span");
+  cardCategory.innerHTML = `<strong>Category</strong>: ${data.categories}`;
+
+  let cardDescription = document.createElement("span");
+
+  let subDescription;
+
+  if (data.description !== undefined) {
+    subDescription = data.description.substring(0, 140);
+  }
+
+  cardDescription.innerHTML = `<strong>Description</strong>: ${subDescription}...`;
+  cardDescription.className = "description-span";
+
+  let moreBtn = document.createElement("button");
+
+  moreBtn.textContent = "See more";
+  moreBtn.addEventListener("click", moreDescription);
+
+  let favourites = document.createElement("button");
+  favourites.textContent = "Add to Favourites";
+  favourites.id = "fav-btn";
+  favourites.addEventListener("click", addToFavourites.bind(null, book));
+
+  function moreDescription(e) {
+    cardDescription.innerHTML = `<strong>Description</strong>: ${data.description}...`;
+    let lessBtn = document.createElement("button");
+    lessBtn.textContent = "Show less";
+    cardDescription.appendChild(lessBtn);
+    lessBtn.addEventListener("click", lessDescription);
+  }
+
+  function lessDescription(e) {
+    cardDescription.innerHTML = `<strong>Description</strong>: ${subDescription}...`;
+    cardDescription.appendChild(moreBtn);
+  }
+
+  card.appendChild(cardImg);
+  card.appendChild(cardTitle);
+  card.appendChild(cardAuthor);
+  card.appendChild(cardPublisher);
+  card.appendChild(cardYear);
+  card.appendChild(cardCategory);
+  card.appendChild(cardDescription);
+  cardDescription.appendChild(moreBtn);
+  card.appendChild(favourites);
+
+  sectionEl.appendChild(card);
+}
+
+function addToFavourites(book) {
+  // getFavourites();
+  const id = book.id;
+  const thumbnail = book.volumeInfo.imageLinks.thumbnail;
+  const title = book.volumeInfo.title;
+  const authors = book.volumeInfo.authors;
+  const publisher = book.volumeInfo.publisher;
+  const publishedDate = book.volumeInfo.publishedDate;
+  const categories = book.volumeInfo.categories;
+  const description = book.volumeInfo.description;
+
+  favouriteBooks.push({
+    thumbnail,
+    title,
+    authors,
+    publisher,
+    publishedDate,
+    categories,
+    description,
+  });
+
+  console.log(favouriteBooks);
+}
+
+function postFavourites() {
+  fetch("http://localhost:5000/api/posts", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(favouriteBooks),
+  }).then((resp) => console.log(resp));
+  // .then((data) => console.log(data));
+}
+
+let test = document
+  .getElementById("test")
+  .addEventListener("click", postFavourites);
+
+function getFavourites() {
+  postFavourites();
+  sectionEl.innerHTML = "";
+  fetch("http://localhost:5000/api/posts")
+    .then((resp) => resp.json())
+    .then((data) => data[0].forEach((book) => createBook(book)));
 }
---
+++ Week 1/Homework/views/home.js
@@ -0,0 +1,17 @@
+const homeTemplate = (context) => context.html`
+    <div>
+    <img
+      src="http://books.google.com/books/content?id=SZtsFe66zucC&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+    /><img />
+    <h2>Google Apps: The Missing Manual</h2>
+    <h3><strong>Authors</strong>: Nancy Conner</h3>
+    <span><strong>Publisher</strong>: "O'Reilly Media, Inc."</span>
+    <span><strong>Date</strong>: 2008-05-27</span>
+    <span><strong>Category</strong>: Computers</span>
+    <span class="description-span">
+    <strong>Description</strong>: Among its many amazing applications, Google
+      now has web-based alternatives to many of the applications in Microsoft
+      Office. This comprehensi...<button>See more</button>
+    </span>
+  </div>
+`;
